e32919d6c53a5ed59f354b80e6e4d8528a2fa040
Jeremy McAnally <jmcanally@github.com>
2013-02-25 09:13:53 -0500

    Fix testing syntax (expected, actual rather than the reverse)

--- test/test_parser.rb
+++ test/test_parser.rb
@@ -12,40 +12,40 @@ class TestParser < MiniTest::Unit::TestCase
   end
   
   def test_string
-    assert_equal @doc["string"], "string\n\t\"string"
+    assert_equal "string\n\t\"string", @doc["string"]
   end
 
   def test_integer
-    assert_equal @doc["integer"], 42
+    assert_equal 42, @doc["integer"]
   end
 
   def test_float
-    assert_equal @doc["pi"], 3.14159
+    assert_equal 3.14159, @doc["pi"]
   end
 
   def test_datetime
-    assert_equal @doc["datetime"], DateTime.iso8601("1979-05-27T07:32:00Z")
+    assert_equal DateTime.iso8601("1979-05-27T07:32:00Z"), @doc["datetime"]
   end
 
   def test_booleans
-    assert_equal @doc["true"], true
-    assert_equal @doc["false"], false
+    assert_equal true, @doc["true"]
+    assert_equal false, @doc["false"]
   end
 
   def test_simple_array
-    assert_equal @doc["arrays"]["simple"], [1, 2, 3]
+    assert_equal [1, 2, 3], @doc["arrays"]["simple"]
   end
 
   def test_nested_array
-    assert_equal @doc["arrays"]["nested"], [[[1], 2], 3]
+    assert_equal [[[1], 2], 3], @doc["arrays"]["nested"]
   end
 
   def test_simple_keygroup
-    assert_equal @doc["e"]["f"], "test"
+    assert_equal "test", @doc["e"]["f"]
   end
 
   def test_nested_keygroup
-    assert_equal @doc["a"]["b"]["c"]["d"], "test"
+    assert_equal "test", @doc["a"]["b"]["c"]["d"]
   end
 
   def test_multiline_arrays
