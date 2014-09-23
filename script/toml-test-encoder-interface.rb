c4d35231ba5dbd174a893b3a02f4ca394e0f79fe
Daniel Paul Searles <daniel@flowboard.com>
2014-09-23 16:47:48 -0700

    Completes #37
    This adds an interface for
    [https://github.com/BurntSushi/toml-test](toml-test). That being said we
    do fail some of the tests and should probably look into fixing the
    failing tests.

---
+++ script/toml-test-encoder-interface.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+#
+# This is an interface for: https://github.com/BurntSushi/toml-test
+
+require 'json'
+require_relative '../lib/toml'
+
+def toml_test_to_ruby(toml_obj)
+  if toml_obj.kind_of?(Array)
+    return toml_obj.map do |value|
+      value = toml_test_to_ruby(value)
+    end
+  elsif toml_obj.has_key?("type")
+    case toml_obj["type"]
+    when "string"
+      return toml_obj["value"]
+    when "integer"
+      return toml_obj["value"].to_i
+    when "float"
+      return toml_obj["value"].to_f
+    when "datetime"
+      return DateTime.iso8601(toml_obj["value"])
+    when "bool"
+      case toml_obj["value"]
+      when "true"
+        return true
+      when "false"
+        return false
+      end
+    when "array"
+      return toml_test_to_ruby(toml_obj["value"])
+    end
+  else
+    obj = {}
+    toml_obj.each do |key, value|
+      obj[key] = toml_test_to_ruby(value)
+    end
+    obj
+  end
+end
+
+obj = toml_test_to_ruby(JSON.load(ARGF.read))
+
+print TOML::Generator.new(obj).body
